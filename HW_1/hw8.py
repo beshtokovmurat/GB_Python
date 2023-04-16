a0fd42d6fa8cfb2f51b5b0789ed313cf0fcfc87b
beshtokovmurat <59954884+beshtokovmurat@users.noreply.github.com>
2023-04-16 14:39:30 +0300

    add HW_1

---
+++ HW_1/hw8.py
@@ -0,0 +1,13 @@
+print("Введите размеры шоколада n*m")
+print("Введите n")
+n= int(input())
+print("Введите m")
+m= int(input())
+print("Введите число к <= n*m")
+k= int(input())
+if (k%n==0) or (k%m==0):
+    print("Yes")
+else:
+    print("No")
+if (k>n*m):
+    print("No")
