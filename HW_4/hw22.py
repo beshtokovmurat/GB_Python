5b79f9185c8b1497fa50cd9cd73ff20188f85670
beshtokovmurat <59954884+beshtokovmurat@users.noreply.github.com>
2023-04-19 00:45:07 +0300

    add hw_4

---
+++ HW_4/hw22.py
@@ -0,0 +1,33 @@
+import random
+
+n1 = int(input("Введите количество 1 последовательности чисел n1 "))
+n2 = int(input("Введите количество 2 последовательности чисел n2 "))
+
+lst1 = list()
+lst2 = list()
+lst3 = list()
+
+k=0;
+
+for i in range(n1):
+    k = random.randint(-5,5)
+    lst1.append(k) 
+print(lst1)
+lst1 = set(lst1)
+print(lst1)
+for i in range(n2):
+    k = random.randint(-5,5)
+    lst2.append(k) 
+print(lst2)
+lst2 = set(lst2)
+print(lst2)
+
+k=0
+for i in lst1:
+    for j in lst2:
+        if i == j:
+            lst3.append(i)
+            k += 1
+print()
+print(sorted(lst3))
+
---
+++ HW_4/hw24.py
@@ -0,0 +1,25 @@
+import random
+
+n = int(input("Введите количество кустов n "))
+
+lst = list()
+
+k=0;
+
+for i in range(n):
+    k = random.randint(0,5)
+    lst.append(k) 
+print(lst)
+max = lst[-3]+lst[-2]+lst[-1]
+while (i < n):
+    if lst[i-2]+lst[i-1]+lst[i] > max:
+        max = lst[i-2]+lst[i-1]+lst[i]
+    i += 1    
+if lst[0]+lst[1]+lst[2] > max:
+   max = lst[0]+lst[1]+lst[2]
+if lst[n-2]+lst[n-1]+lst[0] > max:
+    max = lst[n-2]+lst[n-1]+lst[0]
+if lst[n-1]+lst[0]+lst[1] > max:
+    max = lst[n-1]+lst[0]+lst[1]
+
+print(max)
