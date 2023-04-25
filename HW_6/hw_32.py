efdad00338b18df919023cecdadcdd46125174a9
beshtokovmurat <59954884+beshtokovmurat@users.noreply.github.com>
2023-04-25 22:22:13 +0300

    add hw_6

---
+++ HW_6/hw_32.py
@@ -0,0 +1,15 @@
+# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+lst = [random.randint(-10, 10) for _ in range(10)]
+print(lst)
+
+min = int(input("Введите min число "))
+max = int(input("Введите max число "))
+i = 0 
+for i in range(len(lst)):
+    if min<= lst[i]<= max:
+        print(i, end=" ")
+print()
