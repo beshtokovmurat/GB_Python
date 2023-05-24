3c69601f0ca2351f390dbc1e93e23526b4527728
beshtokovmurat <59954884+beshtokovmurat@users.noreply.github.com>
2023-05-25 02:28:11 +0300

    add hw7

---
+++ HW_7/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# # def print_operation_table(operation, num_rows, num_columns):
+# def print_operation_table(num_rows, num_columns):
+#     for x in range(num_rows):
+#         for y in range(num_columns):
+#             print(lambda x: x+x")
