fc2f314d3768e0560b73ea6c700a40dae01864f4
beshtokovmurat <59954884+beshtokovmurat@users.noreply.github.com>
2023-05-28 21:25:42 +0300

    add hw 3

--- HW_3/hw16,18.py
+++ HW_3/hw16,18.py
@@ -1,27 +1,42 @@
 import random
 n = int(input("Введите количество элементов списка "))
 x = int(input("Введите любое число "))
-list_1 = list()
+lst1 = list()
+lst2 = list()
 for i in range(n): 
     #k = int(input("Введите элементы списка "))
     k = random.randint(0,5)
-    list_1.append(k)
-print(list_1)
-count = 0
+    lst1.append(k)
+    lst2.append(abs(k-x))
+print(lst1)
+print(lst2)
+min = lst2[0]
 i = 0
-while (i < len(list_1)):
-    if x == list_1[i]:
-        count +=1        
+while (i < n):
+    if lst2[i] != 0:
+        min1 = lst2[i]
+        break
     i += 1
-if (count > 0):
-    print(f"\n Число {x} встречается в списке {count} раз(а)")
-min = 0;
-p = 0;
+count = 0
+p = 0
 i = 0
-if count == 0:
-    while (i < len(list_1)):
-        if abs(x - list_1[0]) > abs(x - list_1[i]):
-            min = abs(x - list_1[i])
-            p = i
-        i += 1 
-    print(f"\n Самое близкое число к {x} - это {list_1[p]}")
+p1 = 0
+while (i < n):
+    if x == lst1[i]:
+        count +=1
+    if lst2[i] < min:
+        min = lst2[i]
+        p = i
+    if (lst2[i] != 0):
+        if (lst2[i] < min1):
+            min1 = lst2[i]
+            p1 = i
+    i += 1 
+if (count > 0):
+    print(f"\n Число {x} встречается в списке {lst1} --> {count} раз(а)")
+else:
+    print(f"\n Число {x} отсутствует в списке {lst1}")
+if min == 0:
+    print(f"\n Самое первое близкое число к {x} в списке {lst1} - это {lst1[p1]}")
+else:
+    print(f"\n Самое первое близкое число к {x} в списке {lst1} - это {lst1[p]}")
